f0cea0cc2a3b2a481898834f46b047fa0e228427
LuizFelipe <luizfelipers19@gmail.com>
2021-03-21 22:35:05 -0300

    new user Register method with Password hashing included. Prox commit será referente ao JWT tokenization

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationPlugin;
+using CinemaAPI.Data;
+using CinemaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+        public UsersController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] User user)
+        {
+            var userWithSameEmail = _dbContext.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+
+            if(userWithSameEmail != null)
+            {
+                return BadRequest("User with same email already exists");
+
+            }
+            var userObj = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = SecurePasswordHasherHelper.Hash(user.Password),
+                Role = "Users"
+            };
+
+            _dbContext.Users.Add(userObj);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+    }
+}
