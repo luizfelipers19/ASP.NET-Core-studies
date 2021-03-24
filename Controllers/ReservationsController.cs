fc9742dff624997f68505c87d0ae4f632fd5aad2
LuizFelipe <luizfelipers19@gmail.com>
2021-03-24 09:19:50 -0300

    added Reservation method for users as a POST request

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAPI.Data;
+using CinemaAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+
+        private CinemaDbContext _dbContext;
+
+        public ReservationsController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post([FromBody]Reservation reservationObj)
+        {
+
+            reservationObj.ReservationTime = DateTime.Now;
+
+            _dbContext.Reservations.Add(reservationObj);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+
+        }
+
+
+    }
+}
