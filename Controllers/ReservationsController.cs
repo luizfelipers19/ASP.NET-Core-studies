0672aca8f9539a241d32c0045e5b476f94ef5f77
LuizFelipe <luizfelipers19@gmail.com>
2021-03-24 11:33:12 -0300

    added the Delete method and GetReservationDetails method to the Admin user

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -56,5 +56,62 @@ namespace CinemaAPI.Controllers
 
             return Ok(reservations);
         }
+
+        //retorna os detalhes de uma única reserva
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{id}")]
+        public IActionResult GetReservationDetail(int id)
+        {
+            //usando a sintaxe SQL para juntar as tabelas de reservas com a dos usuários, ligando a chave estrangeira à chave primária correspondente,
+            // e depois juntando a tabela de filmes com a tabela de reservas.
+            // após a junção das tabelas, retornar os atributos desejados com SELECT
+            var reservationDetails = (from reservation in _dbContext.Reservations
+                               join customer in _dbContext.Users on reservation.UserId equals customer.Id
+                               join movie in _dbContext.Movies on reservation.MovieId equals movie.Id
+                               where reservation.Id == id
+                               select new
+                               {
+                                   Id = reservation.Id,
+                                   ReservationTime = reservation.ReservationTime,
+                                   CustomerName = customer.Name,
+                                   MovieName = movie.Name,
+                                   Email = customer.Email,
+                                   Quantity = reservation.Quantity,
+                                   Price = reservation.Price,
+                                   Phone = reservation.Phone,
+                                   PlayingDate = movie.PlayingDate,
+                                   PlayingTime = movie.PlayingTime
+                               }).FirstOrDefault();
+
+            return Ok(reservationDetails);
+        }
+
+
+        // DELETE api/<MoviesController>/5
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var reservation = _dbContext.Reservations.Find(id);
+
+            // caso o objeto não exista/ não tenha sido encontrado pelo Id passado na requisição
+            if (reservation == null)
+            {
+                return NotFound("Mano, tu tá tentando deletar um negócio de um registro que nem existe. Tenta ver certinho aí o número do Id da Reserva que vc tá passando, porque aqui no Servidor não tem nada com esse número");
+            }
+
+            else// caso o registro exista
+            {
+                _dbContext.Reservations.Remove(reservation);
+                _dbContext.SaveChanges();
+
+                return Ok("Registro Deletado mlk");
+
+
+            }
+
+
+        }
     }
 }
